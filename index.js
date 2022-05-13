b30666a2e8159d1a04d3ac525519a7190f0e67fa
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-05-14 01:12:47 +0600

    updte booking

--- index.js
+++ index.js
@@ -18,12 +18,42 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("doctors_portal").collection("services");
+        const bookingCollection = client.db("doctors_portal").collection("bookings");
 
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services)
+        })
+
+        app.get('/available', async (req, res) => {
+            const date = req.query.date || 'May 11, 2022'
+
+            const services = await serviceCollection.find().toArray();
+
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+
+            services.forEach(service => {
+                const serviceBookings = bookings.filter(b => b.treatment === service.name);
+                const booked = serviceBookings.map(s => s.slot);
+                const available = service.slots.filter(s => !booked.includes(s));
+                service.slots = available;
+
+            })
+            res.send(services)
+        })
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({ success: true, result });
         })
     } finally {
 
